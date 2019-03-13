730232549d85145c73d8a1018eb734401772c400
jbrmarks <Student@shc.wisc.edu>
2019-03-13 13:38:31 -0500

    Update: Blank template

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Authored by Joe Marks, 2019
+/*eslint-env browser*/
+
+//initialize function called when the script loads
+function initialize(){
+
+};
+
+
+
+//call the initialize function when the document has loaded
+$(document).ready(initialize);
